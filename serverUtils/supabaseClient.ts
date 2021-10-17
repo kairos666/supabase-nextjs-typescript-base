8c0b4206016e90e46fab14abb1c946efb3cf90d6
magicD <musashi.miyamoto81@gmail.com>
2021-10-17 10:15:57 +0200

    supabase server client (with service key)

---
+++ serverUtils/supabaseClient.ts
@@ -0,0 +1,7 @@
+/* SUPABASE SERVER CLIENT */
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
+
+export const supabase = createClient(<string>supabaseUrl, <string>supabaseServiceKey);
